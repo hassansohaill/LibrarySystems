4fa42c54944054c039d133d15219de09a2200841
devont9122 <devon.tran@ucalgary.ca>
2023-04-16 13:37:58 -0600

    Profile page, overdue homepage component, create account pages

---
+++ test/test/Data/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace test.Data
+{
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Status { get; set; }
+        public string Image { get; set; }
+        public string Type { get; set; }
+        public int num { get; set; }
+        public string Description { get; set; }
+
+    }
+}
+
---
+++ test/test/Models/ModelTwo.cs
@@ -0,0 +1,32 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+public class ModelTwo
+{
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Confirm email is required.")]
+    [Compare("Email")]
+    [Display(Name = "Confirm Email")]
+    public string ConfirmEmail { get; set; }
+
+    [Required(ErrorMessage = "PIN is required.")]
+    [RegularExpression("^[0-9]{6}$", ErrorMessage = "Pin must be 6 digits from [0-9]")]
+    public string Pin { get; set; }
+
+    [Required(ErrorMessage = "Confirm PIN is required.")]
+    [Compare("Pin", ErrorMessage = "PIN and Confirm PIN must match.")]
+    [Display(Name = "Confirm Pin")]
+    public string ConfirmPin { get; set; }
+
+    [Required(ErrorMessage = "You must accept the terms and conditions.")]
+    [Range(typeof(bool), "true", "true", ErrorMessage = "Accept Terms & Conditions is required")]
+    public bool AcceptTerms { get; set; }
+
+
+
+}
