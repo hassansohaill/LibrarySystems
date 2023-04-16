9fcfea62203a2a1556a96e6634f053b7d1e55b09
devont9122 <devon.tran@ucalgary.ca>
2023-04-16 14:49:04 -0600

    Hopefully this works T-T

---
+++ test/test/Data/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+
+
+namespace test.Data
+{
+    public class User
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public DateTime DOB;
+        public long CardNum;
+        public int Pin;
+        public List<Tuple<DateTime, Book>> DueDates = new List<Tuple<DateTime, Book>>();
+
+
+        //class constructor
+        public User(string First, string Last, string Mail, DateTime Birth, int PinNum)
+        {
+            //for creating random card number
+            Random RandNum = new Random();
+            string CardString = string.Empty;
+
+            for (int i = 0; i < 14; i++)
+            {
+                CardString = String.Concat(CardString, RandNum.Next(10).ToString());
+
+            }
+
+            CardNum = Convert.ToInt64(CardString);
+
+            FirstName = First;
+            LastName = Last;
+            Email = Mail;
+            DOB = Birth;
+            Pin = PinNum;
+
+        }
+
+        public void addSignedOutBook(DateTime Due, Book BookSignedOut)
+        {
+            DueDates.Add(new Tuple<DateTime, Book>(Due, BookSignedOut));
+        }
+
+    }
+
+}
---
+++ test/test/Models/ModelOne.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+public class ModelOne
+{
+    [Required(ErrorMessage = "First name is required.")]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required.")]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Date of birth is required.")]
+    [Display(Name = "Date of Birth")]
+    public DateTime? DateOfBirth { get; set; }
+
+
+}
